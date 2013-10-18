976f360eb7f62e3a72bed958d412f9c2e68dee68
JCPedroza <nemesis4go10@hotmail.com>
2013-10-18 09:18:37 -0600

    more tests

--- tests.js
+++ tests.js
@@ -4,17 +4,17 @@ var sounds = require("./sounds");
 
 // ===== declarations =====
 
-var A  = new sounds.Note("A",  440.000, 4);
+var A  = new sounds.Note("A",  440.000, 4, "La");
 var Bb = new sounds.Note("Bb", 466.164, 4);
-var B  = new sounds.Note("B",  493.883, 4);
-var C  = new sounds.Note("C",  523.251, 5);
+var B  = new sounds.Note("B",  493.883, 4, "Si");
+var C  = new sounds.Note("C",  523.251, 5, "Do");
 var Db = new sounds.Note("Db", 554.365, 5);
-var D  = new sounds.Note("D",  587.330, 5);
+var D  = new sounds.Note("D",  587.330, 5, "Re");
 var Eb = new sounds.Note("Eb", 622.254, 5);
 var E  = new sounds.Note("E",  659.255, 5);
 var F  = new sounds.Note("F",  698.456, 5);
 var Gb = new sounds.Note("Gb", 739.989, 5);
-var G  = new sounds.Note("G",  783.991, 5);
+var G  = new sounds.Note("G",  783.991, 5, "Sol");
 var Ab = new sounds.Note("Ab", 830.609, 5);
 
 var A2  = new sounds.Note("A",  440.000, 4);
@@ -29,7 +29,7 @@ var BCD = new sounds.NoteCollection([B, C, D], "BCD"); // use only at NoteCollec
 console.assert(A.name === "A", "1");
 console.assert(A.freq === 440.000, "2");
 console.assert(A.octave === 4, "3");
-console.assert(A.toString() === "name=A freq=440 octave=4", "4");
+console.assert(A.toString() === "name=A freq=440 octave=4 name2=La", "4");
 
 // NoteCollection getters
 console.assert(ABC.size === 3, "5");
@@ -75,7 +75,29 @@ BCD.setNotes([C, A, B, A2, A3, G, C, A2]);
 BCD.removeNotesWithFreq(440);
 console.assert(BCD.getNotesAsString() === "C B G C ", "31");
 console.assert(BCD.size === 4, "32");
-
-
+BCD.removeNotesWithName2("Do");
+console.assert(BCD.getNotesAsString() === "B G ", "33");
+console.assert(BCD.size === 2, "34");
+BCD.reset();
+console.assert(BCD.size === 3, "35");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([B, C, D]), "36");
+BCD.rotateBack();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([D, B, C]), "37");
+BCD.rotateBack();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([C, D, B]), "38");
+BCD.rotateBack();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([B, C, D]), "39");
+BCD.rotate();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([C, D, B]), "40");
+BCD.rotate();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([D, B, C]), "41");
+BCD.rotate();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([B, C, D]), "42");
+BCD.setNotes([A, B, C, D, E, F, G]);
+console.assert(BCD.getSize() === 7, "43");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([A, B, C, D, E, F, G]), "44");
+BCD.removeNotesWithFreqRange(490, 700);
+console.assert(BCD.getSize() === 2, "45");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([A, G]), "46");
 
 
