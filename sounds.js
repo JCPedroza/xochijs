8dbb350f60f543fb126f82813a491aaf25f51466
JCPedroza <nemesis4go10@hotmail.com>
2013-10-17 21:19:33 -0600

    implemented Note and NoteCollection, think about other methods you can implement, still need to implement Scale and Chord subclasses

--- sounds.js
+++ sounds.js
@@ -1,14 +1,155 @@
-function Note(name, octave, freq){
-    // instance variables
+function Note(name, freq, octave){
     this.name   = name;
     this.octave = typeof octave === "undefined" ? 0 : octave;  // default argument is 0
     this.freq   = typeof freq   === "undefined" ? 0 : freq;    // default argument is 0
     
-    // methods
+    /** Returns the object state as a string. */
     this.toString = function(){
-        return "name: " + this.name + " octave: " + this.octave + "freq: " + this.freq;
+        return "name=" + this.name + " freq=" + this.freq + " octave=" + this.octave;
     };
 }
 
-// node export
+function NoteCollection(notes, name, name2){
+    this._notes  = notes;                                      // array of notes
+    this._notes2 = notes.slice(0);                             // original notes (clone)
+    this.name    = typeof name  === "undefined" ? "" : name;   // default argument is ""
+    this.name2   = typeof name2 === "undefined" ? "" : name2;  // default argument is ""
+    this.size    = notes.length;
+    
+    // ==================================
+    //             setters
+    // ==================================
+
+    /** Change the notes. */ // !!! Should this implement variable arguments?
+    this.setNotes = function(notes){
+        this._notes = notes;
+        this.size   = this._notes.length;
+    };
+    
+    /** Adds one note. */
+    this.addNote = function(note){
+        this._notes.push(note);
+        this.size++;
+    };
+    
+    /** Changes the name of the NoteCollection object. */
+    this.setName = function(newName){
+        this.name = newName;
+    };
+
+    /** Changes the secondary name of the NoteCollection object. */
+    this.setName2 = function(newName){
+        this.name2 = newName;
+    };
+
+    /** Resets notes to its original state (notes2). */
+    this.reset = function(){
+        this._notes = this._notes2.slice(0);
+        this.size   = this._notes.length;
+    };
+
+    /** Sends the first note to the last index. */  // !!! Can this be more efficient?
+    this.rotate = function(){
+        this._notes.push(this._notes.shift());
+    };
+    
+    /** Reverses the order of the notes. */
+    this.reverse = function(){
+        this._notes.reverse();
+    };
+
+    /** Removes the note at given index. */
+    this.removeNoteAt = function(index){
+        this._notes.splice(index, 1);
+        this.size--;
+    };
+
+    /** !!! Removes all the notes with given name. */
+    this.removeNotesWithName = function(name){
+        this._notes = this._notes.filter(
+            function(element){
+                return element.name !== name;
+            });
+        this.size = this._notes.length;
+    };
+
+    /** !!! Removes all the notes with given frequency. */
+    this.removeNotesWithFreq = function(freq){
+        this._notes = this._notes.filter(
+            function(element){
+                return element.freq !== freq;
+            });
+        this.size = this._notes.length;
+    };
+
+    // ==================================
+    //             getters
+    // ==================================
+    
+    /** Returns the object state as a string */
+    this.toString = function(){
+        var returnString = "size=" + this.size + " name=" + this.name + " name2=" + this.name2;
+        returnString += "\nnotes=";
+        for (var i in this._notes){
+            returnString += " <";
+            returnString += this._notes[i].toString();
+            returnString += ">";
+        }
+        returnString += "\nnotes2=";
+        for (var j in this._notes2){
+            returnString += " <";
+            returnString += this._notes2[j].toString();
+            returnString += ">";
+        }
+        return returnString;
+    };
+
+    /** Returns the notes. */
+    this.getNotes = function(){
+        return this._notes;
+    };
+
+    /** Returns the original notes. */
+    this.getOriginalNotes = function(){
+        return this._notes2;
+    };
+
+    /** Returns name. */
+    this.getName = function(){
+        return this.name;
+    };
+
+    /** Returns secondary name (name2) */
+    this.getName2 = function(){
+        return this.name2;
+    };
+
+    /** Returns the name of the notes as a string. */
+    this.getNotesAsString = function(){
+        var returnString = "";
+        for (var i in this._notes)
+            returnString += this._notes[i].name + " ";
+        return returnString;
+    };
+
+    /** Returns the name of the original notes as a string. */
+    this.getOriginalNotesAsString = function(){
+        var returnString = "";
+        for (var i in this._notes2)
+            returnString += this._notes2[i].name + " ";
+        return returnString;
+    };
+
+    /** Returns the frequencies of the notes as a string. */
+    this.getFreqsAsString = function(){
+        var returnString = "";
+        for (var i in this._notes)
+            returnString += this._notes[i].freq + " ";
+        return returnString;
+    };
+
+}
+
+// node exports
 exports.Note = Note;
+exports.NoteCollection = NoteCollection;
---
+++ tests.js
@@ -0,0 +1,81 @@
+// ===== imports =====
+
+var sounds = require("./sounds");
+
+// ===== declarations =====
+
+var A  = new sounds.Note("A",  440.000, 4);
+var Bb = new sounds.Note("Bb", 466.164, 4);
+var B  = new sounds.Note("B",  493.883, 4);
+var C  = new sounds.Note("C",  523.251, 5);
+var Db = new sounds.Note("Db", 554.365, 5);
+var D  = new sounds.Note("D",  587.330, 5);
+var Eb = new sounds.Note("Eb", 622.254, 5);
+var E  = new sounds.Note("E",  659.255, 5);
+var F  = new sounds.Note("F",  698.456, 5);
+var Gb = new sounds.Note("Gb", 739.989, 5);
+var G  = new sounds.Note("G",  783.991, 5);
+var Ab = new sounds.Note("Ab", 830.609, 5);
+
+var A2  = new sounds.Note("A",  440.000, 4);
+var A3  = new sounds.Note("A",  440.000, 4);
+
+var ABC = new sounds.NoteCollection([A, B, C], "ABC");
+var BCD = new sounds.NoteCollection([B, C, D], "BCD"); // use only at NoteCollection mutation
+
+// ===== assertions =====
+
+// Note getters
+console.assert(A.name === "A", "1");
+console.assert(A.freq === 440.000, "2");
+console.assert(A.octave === 4, "3");
+console.assert(A.toString() === "name=A freq=440 octave=4", "4");
+
+// NoteCollection getters
+console.assert(ABC.size === 3, "5");
+console.assert(ABC.name === "ABC", "6");
+console.assert(ABC.name2 === "", "7");
+console.assert(JSON.stringify(ABC.getNotes()) === JSON.stringify([A, B, C]), "8");
+
+// NoteCollection mutation
+BCD.addNote(E);
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([B, C, D, E]), "9");
+console.assert(BCD.size === 4, "10");
+BCD.reset();
+console.assert(BCD.size === 3, "11");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([B, C, D]), "12");
+BCD.setNotes([D, E]);
+console.assert(BCD.size === 2, "13");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([D, E]), "14");
+BCD.reset();
+console.assert(BCD.size === 3, "15");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([B, C, D]), "16");
+BCD.reverse();
+console.assert(BCD.size === 3, "17");
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([D, C, B]), "18");
+BCD.rotate();
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([C, B, D]), "19");
+BCD.removeNoteAt(1);
+console.assert(JSON.stringify(BCD.getNotes()) === JSON.stringify([C, D]), "20");
+console.assert(BCD.size === 2, "21");
+console.assert(BCD.getNotesAsString() === "C D ", "22");
+console.assert(BCD.getOriginalNotesAsString() === "B C D ", "23");
+console.assert(BCD.getFreqsAsString() === "523.251 587.33 ", "24");
+BCD.setName("C D");
+BCD.setName2("secondary name");
+console.assert(BCD.getName() === "C D", "25");
+console.assert(BCD.getName2() === "secondary name", "26");
+BCD.setNotes([C, A, B, A2, A3, G, C, A2]);
+console.assert(BCD.getNotesAsString() === "C A B A A G C A ", "27");
+console.assert(BCD.size === 8, "28");
+BCD.removeNotesWithName("A");
+console.assert(BCD.getNotesAsString() === "C B G C ", "29");
+console.assert(BCD.size === 4, "30");
+BCD.setNotes([C, A, B, A2, A3, G, C, A2]);
+BCD.removeNotesWithFreq(440);
+console.assert(BCD.getNotesAsString() === "C B G C ", "31");
+console.assert(BCD.size === 4, "32");
+
+
+
+
