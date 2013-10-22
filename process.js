a7178ed6ad1037a285efe15a480a7e4a2012d145
JCPedroza <nemesis4go10@hotmail.com>
2013-10-22 11:59:06 -0600

    implemented harmonize)

--- process.js
+++ process.js
@@ -24,6 +24,28 @@ var scalize = function(note, formula, pool){
     return new sounds.Scale(noteArray);
 };
 
+/** Forms chords derived from the input scale, using superimposed thirds. */
+var harmonize = function(scale, depth){
+    if (depth < 1 || typeof depth !== "number")
+        throw new Error("depth must be a number > 0");
+    var chordArray = [];
+    var notes      = scale.getNotes();
+    var scaleSize  = scale.getSize();
+    for(var i = 0; i < scaleSize; i++){                  // For every note in the scale:
+        var aNoteArray = [];                             // Build a note array (a chord):
+        for(var j = 0, k = 0; j < depth; j++, k += 2)    // With that note plus a number -
+            aNoteArray[j] = notes[(k + i) % scaleSize];  // of superimposed thirds (depth).
+        chordArray.push(new sounds.Chord(aNoteArray));   // Add that chord to chordList.
+    }
+    return new sounds.Harmony(chordArray);
+};
+
+/** Identifies the name of a chord. */
+var identifyChord = function(chord){
+    return "!!!"
+};
+
 // Node exports:
 exports.stepCount = stepCount;
 exports.scalize   = scalize;
+exports.harmonize = harmonize;
