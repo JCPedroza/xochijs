7e405c535586ff1ed8c5c4504ba8d32a58e9afef
JCPedroza <nemesis4go10@hotmail.com>
2013-10-23 11:40:39 -0600

    implemented harmonize0

--- harmony.js
+++ harmony.js
@@ -1,14 +1,33 @@
 var sounds = require("./sounds");
 
-/** Forms chords derived from the input scale, using superimposed thirds. */
+/** 
+* Forms chords derived from the input scale, using superimposed thirds.
+* 
+*/
 var harmonize = function(scale, depth, option){
     if (depth < 1 || typeof depth !== "number")
         throw new Error("depth must be a number > 0");
-    var mode       = option || 0;
+    var mode = option || 0;
     if (mode === 0) return harmonize0(scale, depth);
+    if (mode === 1) return harmonize1(scale, depth);
 };
 
+// Helper for harmonize(), returns an Harmony object.
 var harmonize0 = function(scale, depth){
+    var chordArray = [];
+    var notes      = scale.getNotes();
+    var scaleSize  = scale.getSize();
+    for (var i = 0; i < scaleSize; i++){                 // For every note in the scale:
+        var aNoteArray = [];                             // Build a note array (a chord):
+        for(var j = 0, k = 0; j < depth; j++, k += 2)    // With that note plus a number -
+            aNoteArray[j] = notes[(k + i) % scaleSize];  // of superimposed thirds (depth).
+        chordArray.push(new sounds.Chord(aNoteArray));   // Add that chord to chordList.
+    }
+    return new sounds.Harmony(chordArray);
+};
+
+// Helper for harmonize(), returns an Harmony object.
+var harmonize1 = function(scale, depth){
     var chordArray = [];
     var notes      = scale.getNotes();
     var scaleSize  = scale.getSize();
