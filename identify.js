221ad2468e400017907281b81953b6838fd7b373
JCPedroza <nemesis4go10@hotmail.com>
2013-11-06 04:41:40 -0700

    helper function names now start with an underscore

--- identify.js
+++ identify.js
@@ -9,7 +9,7 @@
 var chord = function(chord){
     var chordSize    = chord.getSize();
     var permutations = process.buildPermutations(chord);                                   // Array with all the permutations of the chord.
-    var formula      = getFormulas(chordSize);                                             // Determine a formula object to use.
+    var formula      = _getFormulas(chordSize);                                             // Determine a formula object to use.
     var returnArray  = [];                                                                 // Array that will be populated with possible names.
     for (var i = 0; i < permutations.length; i++){
         var current = permutations[i].toFormula().slice(0, -1);                            // Last value in formula is not relevant. (1)
@@ -17,7 +17,7 @@ var chord = function(chord){
         for (var key in formula){                                                          // Loop through chord formulas.
             if (formula.hasOwnProperty(key)){                                              // Checks property doesn't come from prototype.
                 if (process.arraysEqual(current, formula[key][0]))                         // Checks for a match.
-                    returnArray.push(determineRoot(lowest, formula[key][1]) + " " + key);  // Add lowest note and key if there is a match.
+                    returnArray.push(_determineRoot(lowest, formula[key][1]) + " " + key);  // Add lowest note and key if there is a match.
             }
         }
     }
@@ -25,7 +25,7 @@ var chord = function(chord){
 };
 
 // Helper for chord(). Determines formula object based on chord size.
-var getFormulas = function(chordSize){
+var _getFormulas = function(chordSize){
     if      (chordSize  <  2) throw new Error("chordSize must be bigger than 1");
     else if (chordSize === 2) return formulas.twoNoteChords;
     else if (chordSize === 3) return formulas.threeNoteChords;
@@ -33,8 +33,8 @@ var getFormulas = function(chordSize){
     else if (chordSize === 5) return formulas.fiveNoteChords;
 };
 
-// Helper for chords(). Determines a root note for the formula.
-var determineRoot = function(lowest, offset){
+// Helper for chords(). Determines a root note of the formula. It is used for no root chords.
+var _determineRoot = function(lowest, offset){
     if (offset === 0) return lowest;
     var pool          = formulas.ET12POOL;
     var indexOfLowest = pool.indexOf(lowest);
