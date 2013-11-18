31504099f40d130f22945a64aea953b9460ff2d8
JCPedroza <nemesis4go10@hotmail.com>
2013-11-17 21:44:24 -0700

    arraysEqual can now use X.equals() method

--- processing.js
+++ processing.js
@@ -9,11 +9,23 @@ var formulas = require("./formulas");
 
 /** Checks equality between two arrays. */  // needs to handle nested arrays
 var arraysEqual = function(a, b) {
-  if (a === b)                  return true;
-  if (a === null || b === null) return false;
-  if (a.length != b.length)     return false;
-  for (var i = 0; i < a.length; ++i)
-    if (a[i] !== b[i]) return false;
+    var i;
+    if (a === b)                  return true;
+    if (a === null || b === null) return false;
+    if (a.length != b.length)     return false;
+    if (typeof a[0] !== "object"){
+        for (i = 0; i < a.length; i += 1){
+            if (a[i] !== b[i]) {
+                return false;
+            }
+        }
+    } else {
+        for (i = 0; i < a.length; i += 1){
+            if (!a[i].equals(b[i])) {
+                return false;
+            }
+        }
+    }
   return true;
 };
 
