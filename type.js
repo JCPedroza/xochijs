f5076cd1ea001473bbd882fb1f9fb3aef3d9ff71
JCPedroza <nemesis4go10@hotmail.com>
2013-11-28 06:01:01 -0700

    more type checks

--- type.js
+++ type.js
@@ -2,12 +2,25 @@
 /* jslint node: true */
 "use strict";
 
+// Imports:
+
+var sounds = require("./sounds");
+
 // ===========================================
 //                checkString 
 // ===========================================
 var checkString = function (arg) {
-    if (typeof arg !== "string") {
+    if (typeof arg !== "string" && typeof arg !== "undefined") {
         throw new TypeError("argument must be of type string");
+    }
+};
+
+// ===========================================
+//                checkArray
+// ===========================================
+var checkArray = function (arg) {
+    if (!Array.isArray(arg) && typeof arg !== "undefined") {
+        throw new TypeError("argument must be of type Array");
     }
 };
 
@@ -15,12 +28,52 @@ var checkString = function (arg) {
 //             checkStringArray 
 // ===========================================
 var checkStringArray = function (arg) {
-    if (!(arg instanceof Array) || typeof arg[0] !== "string"){
+    if ((!Array.isArray(arg) && typeof arg !== "undefined") ||
+    (typeof arg[0] !== "string")) {
         throw new TypeError("argument must be an array of string");
     }
 };
 
+// ===========================================
+//             checkNumberArray 
+// ===========================================
+var checkNumberArray = function (arg) {
+    if ((!Array.isArray(arg) && typeof arg !== "undefined") ||
+    (typeof arg[0] !== "number")) {
+        throw new TypeError("argument must be an array of number");
+    }
+};
+
+// ===========================================
+//             checkNoteCollection
+// ===========================================
+var checkNoteCollection = function (arg) {
+    if (!(arg instanceof sounds.NoteCollection)) {
+        throw new TypeError("argument must be of type NoteCollection");
+    }
+};
+
+// ===========================================
+//                checkNote
+// ===========================================
+var checkNote = function (arg) {
+    if (!(arg instanceof sounds.Note)) {
+        throw new TypeError("argument must be of type Note");
+    }
+};
 
 // node exports
-exports.checkString      = checkString;
-exports.checkStringArray = checkStringArray;
+exports.checkString         = checkString;
+exports.checkArray          = checkArray;
+exports.checkStringArray    = checkStringArray;
+exports.checkNumberArray    = checkNumberArray;
+exports.checkNoteCollection = checkNoteCollection;
+exports.checkNote           = checkNote;
+
+
+// -----------------
+//      Notes
+// -----------------
+
+// Undefined check: used to handle optional arguments.
+
