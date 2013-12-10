7b85a20eabaac36d10015f525e0d14f782bc5ae2
JCPedroza <nemesis4go10@hotmail.com>
2013-12-10 00:53:44 -0700

    implemented pool check

--- type.js
+++ type.js
@@ -62,13 +62,35 @@ var checkNote = function (arg) {
     }
 };
 
+// ===========================================
+//            checkNoteNameInPool
+// ===========================================
+var checkNoteNameInPool = function (arg, pool) {
+    if (pool.indexOf(arg) === -1) {
+        throw new Error("note name " + arg + " not found in pool " + pool);
+    }
+};
+
+// ===========================================
+//          checkNoteNameGroupInPool
+// ===========================================
+var checkNoteNameGroupInPool = function (args, pool) {
+    var index,
+        length = args.length;
+    for (index = 0; index < length; index += 1) {
+        checkNoteNameInPool(args[index], pool);
+    }
+};
+
 // node exports
-exports.checkString         = checkString;
-exports.checkArray          = checkArray;
-exports.checkStringArray    = checkStringArray;
-exports.checkNumberArray    = checkNumberArray;
-exports.checkNoteCollection = checkNoteCollection;
-exports.checkNote           = checkNote;
+exports.checkString              = checkString;
+exports.checkArray               = checkArray;
+exports.checkStringArray         = checkStringArray;
+exports.checkNumberArray         = checkNumberArray;
+exports.checkNoteCollection      = checkNoteCollection;
+exports.checkNote                = checkNote;
+exports.checkNoteNameInPool      = checkNoteNameInPool;
+exports.checkNoteNameGroupInPool = checkNoteNameGroupInPool;
 
 
 // -----------------
